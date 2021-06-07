e4266a16689fbfeba6bb9f77d712c7c0bea532d7
Pedro Da <>
2021-06-07 17:43:37 +0000

    Parte dos exercícios que fiz semana passada

--- haskell04.hs
+++ haskell04.hs
@@ -1,2 +1,27 @@
 -- Prática 04 de Haskell
 -- Nome: 
+
+faixaIdoso :: Int -> String
+faixaIdoso idade = 
+  if idade >= 80 then "IDO80"
+  else if idade >= 75 then "IDO79"
+  else if idade >= 70 then "IDO74"
+  else if idade >= 65 then "IDO69"
+  else if idade >= 60 then "IDO64"
+  else "ND"
+
+classifIdosos :: [(String,Int)] -> [(String,Int,String)]
+classifIdosos ltupla = zipWith (\(x,y) z -> (x,y,z)) ltupla [faixaIdoso (snd x) | x <- ltupla]
+
+classifIdosos' :: [(String,Int)] -> [(String,Int,String)]
+classifIdosos' ltupla = zipWith (\(x,y) z -> (x,y,z)) ltupla (map faixaIdoso (map (\(_,x) -> x) ltupla)) 
+
+--juntaTuplas :: (Int, String) -> (Int, String, Int)
+--juntaTuplas tupla = (\(x,y) z -> (x,y,z)) tupla 10
+
+strColor :: (Int,Int,Int) -> String
+strColor tuple = (\(x,y,z) -> "rgb(" ++ show x ++ "," ++ show y ++ "," ++ show z ++ ")") tuple
+
+genCircs :: Int -> (Int,Int) -> Int -> [(Int,Int,Int)]
+genCircs qtd coord raio =  ((\z (x,y) -> (x,y,z))raio) coord 
+--take qtd (iterate (raio+) )
